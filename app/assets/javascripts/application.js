bae2fb921aec8e1b1c7814c454891f6f41888343
Thomas English <thomaswenglish@gmail.com>
2016-04-28 10:48:29 -0400

    itpnew

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery.raptorize.1.0.js
 //= require jquery_ujs
 //= require lib/modernizr-2.6.2.min
 //= require lib/plugins

325e31158a164c18842202d968e12f281e33bb94
Thomas English <tenglish@rtop.com>
2016-04-28 18:07:06 -0400

    animate

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
+    $('.logos').hover(function() {
+        $('#bottom').animate({top: '75%'}, 1000, function(){
 
-    $(".logos").hover(function(){
-        $("#bottom").animate({width: 'toggle'})
-    });
-
+        });
+    },function(){
+        $('#bottom').animate({top:"100%"}, 1000, function(){
+     });
+ });
 });
+
 

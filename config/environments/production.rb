7b70fd60a883e7f212e697714e03e7f2560a3894
Thomas English <tenglish@rtop.com>
2016-04-28 18:08:39 -0400

    animate1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,5 +62,9 @@ Boilerplate::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
 end

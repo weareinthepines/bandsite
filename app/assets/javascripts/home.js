fb537556a8da08097d740d6b287b6ad3bd7c9706
Thomas English <tenglish@rtop.com>
2016-05-18 17:27:55 -0400

    vh

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,21 +1,57 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var INITIAL_LOAD = true;
 
-$(document).ready(function(){
+$(document).ready(function() {
+    $('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
 
-    $('.window').windows({
-        snapping: true,
-        snapSpeed: 500,
-        snapInterval: 1100,
-        onScroll: function(scrollPos){
-            // scrollPos:Number
-        },
-        onSnapComplete: function($el){
-            // after window ($el) snaps into place
-        },
-        onWindowEnter: function($el){
-            // when new window ($el) enters viewport
+    scrollIntervalID = setInterval(stickIt, 10);
+
+
+    function stickIt() {
+
+
+        var orgElementPos = $('.original').offset();
+        orgElementTop = orgElementPos.top;
+
+        if ($(window).scrollTop() >= (orgElementTop)) {
+            // scrolled past the original position; now only show the cloned, sticky element.
+
+            // Cloned element should always have same left position and width as original element.
+            orgElement = $('.original');
+            coordsOrgElement = orgElement.offset();
+            leftOrgElement = coordsOrgElement.left;
+            widthOrgElement = orgElement.css('width');
+            $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
+            $('.original').css('visibility', 'hidden');
+        } else {
+            // not scrolled past the menu; only show the original menu.
+            $('.cloned').hide();
+            $('.original').css('visibility', 'visible');
         }
-    })
+    }
+
 
 });
+
+$(document).ready(function() {
+    function logoHeight() {
+        var $logo = $(".logos"),
+            $header = $("#bnbutton"),
+            windowHeight = $(window).height(),
+            heightDifference = windowHeight - $logo.height(),
+            anotherHeight = heightDifference/2,
+            finalHeight = anotherHeight - $header.height();
+
+        $logo.css({
+            top: finalHeight
+        })
+    }
+
+    logoHeight();
+
+    $(window).resize(function(){
+        logoHeight();
+    });
+});
+

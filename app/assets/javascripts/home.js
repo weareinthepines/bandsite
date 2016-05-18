4d07851da09b9b334e1e74d449eb4d8209a27e00
Thomas English <tenglish@rtop.com>
2016-05-18 14:00:56 -0400

    1page1

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-    $('.logos').hover(function() {
-        $('#bottom').animate({top: '75%'}, 1000, function(){
-
-        });
-    },function(){
-        $('#bottom').animate({top:"100%"}, 1000, function(){
-     });
- });
-});
 
 $(document).ready(function(){
-    $('.fading').fadeIn();
+
+    $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 1100,
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el){
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+        }
+    })
+
 });

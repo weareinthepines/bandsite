b469c291e13a983dea2ba8274cdadb3254830733
Thomas English <tenglish@rtop.com>
2016-05-19 15:06:27 -0400

    mobile

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,7 @@
 var INITIAL_LOAD = true;
 
 $(document).ready(function() {
-    $('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
+    $('.social').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
 
     scrollIntervalID = setInterval(stickIt, 10);
 
@@ -20,9 +20,8 @@ $(document).ready(function() {
             // Cloned element should always have same left position and width as original element.
             orgElement = $('.original');
             coordsOrgElement = orgElement.offset();
-            leftOrgElement = coordsOrgElement.left;
             widthOrgElement = orgElement.css('width');
-            $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
+            $('.cloned').css('top', 0).css('width', widthOrgElement).show();
             $('.original').css('visibility', 'hidden');
         } else {
             // not scrolled past the menu; only show the original menu.
@@ -49,10 +48,6 @@ $(document).ready(function() {
     }
 
     logoHeight();
-
-    $(window).resize(function(){
-        logoHeight();
-    });
 });
 
 $(document).ready(function() {
@@ -69,19 +64,20 @@ $(document).ready(function() {
 
     showHeight();
 
-    $(window).resize(function(){
-        showHeight();
-    });
 });
 
-$(document).ready(function() {
-    function midScroll () {
-        var $foreverGreen = $("#foreverGreen")
-        $foreverGreen.scrollSpeed(1000, 800)
-    }
-
-    midScroll()
-
-
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
 
